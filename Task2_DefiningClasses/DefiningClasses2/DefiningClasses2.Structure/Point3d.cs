1870eb50f2e906c103649833bb8d4c7ebb48ec63
adachenski <adachenski@aol.com>
2016-05-27 23:47:00 +0300

    Task2

---
+++ Task2_DefiningClasses/DefiningClasses2/DefiningClasses2.Structure/Point3d.cs
@@ -0,0 +1,46 @@
+﻿namespace DefiningClasses2.Structure
+{
+    using System;
+
+    public struct Point3D
+    {
+        private double x;
+        private double y;
+        private double z;
+        private static readonly Point3D o;
+
+        public double X
+        {
+            get { return this.x; }
+            set { this.x = value; }
+        }
+        public double Y
+        {
+            get { return this.y; }
+            set { this.y = value; }
+        }
+
+        public double Z
+        {
+            get { return this.z; }
+            set { this.z = value; }
+        }
+
+        public static Point3D O
+        {
+            get { return o; }
+        }
+
+        public Point3D(double x) : this()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return String.Format("X = {0}, Y = {1}, Z = {2}", X, Y, Z);
+        }
+
+    }
+
+}
---
+++ Task2_DefiningClasses/DefiningClasses2/DefiningClasses2/Start.cs
@@ -0,0 +1,15 @@
+﻿namespace DefiningClasses2
+{
+    using Structure;
+    using System;
+
+    public class Start
+    {
+        static void Main(string[] args)
+        {
+            Point3D myPoint = new Point3D() { X = 5, Y = 32, Z = 43 };
+         
+            Console.WriteLine(myPoint.ToString());
+        }
+    }
+}
