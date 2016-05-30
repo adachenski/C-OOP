d812aa1e26aa3ef19152362afa24ed59297f58f8
adachenski <adachenski@aol.com>
2016-05-30 23:21:03 +0300

    Few more methods implemented

--- Task2_DefiningClasses/DefiningClasses2/DefiningClasses2.Structure/GenericList.cs
+++ Task2_DefiningClasses/DefiningClasses2/DefiningClasses2.Structure/GenericList.cs
@@ -6,48 +6,97 @@
 
     public class GenericList<T>
     {
-        const int DefaultCapacity = 10;
-        public List<T> listOfElements;
-        private T[] ElementContainer;
-        private int count = 0;
-        private int currentCapacity { get; set; }
+        public T[] elementContainer;
+        int nextIndex = 0;
+        private int initialValue { get; set; }
 
-        public GenericList(int capacity)
+        public GenericList(int initialSize)
         {
-            this.currentCapacity = capacity;
-            this.ElementContainer = new T[capacity];
-            this.listOfElements = new List<T>(capacity);
+            this.initialValue = initialSize;
+            elementContainer = new T[initialSize];
         }
+
         public void AddElement(T element)
         {
-            if (listOfElements.Count > currentCapacity-1)
+            elementContainer[this.nextIndex] = element;
+            elementContainer = AutoGrow(elementContainer);
+            nextIndex++;
+        }
+
+        public T GetElement(int index)
+        {
+            return elementContainer[index];
+        }
+
+        public void RemovingElement(int index)
+        {
+            for (int i = index; i < elementContainer.Length - 1; i++)
             {
-                Console.WriteLine("Maximu capacity of {0} reached!!!",currentCapacity);
-                Console.WriteLine("Element Not Added!!!");
-                return;
+                elementContainer[i] = this.elementContainer[i + 1];
+            }
+            this.nextIndex--;
+            this.elementContainer[nextIndex] = default(T);
+        }
+
+
+        public void InsertElement(int index, T newElement)
+        {
+
+            for (int i = nextIndex; i > index && i > 0; i--)
+            {
+                this.elementContainer[i] = this.elementContainer[i - 1];
+            }
+            this.elementContainer[index] = newElement;
+            this.elementContainer = AutoGrow(elementContainer);
+            this.nextIndex++;
+        }
+        public void ClearList()
+        {
+            this.elementContainer = new T[initialValue];
+        }
+        public T FindElement(T value)
+        {
+            foreach (var item in elementContainer)
+            {
+                if (item.Equals(value))
+                {
+                    return item;
+                }
+            }
+            Console.WriteLine("Your Item was not found!!!");
+            return value;
+        }
+        public T[] AutoGrow(T[] valueHolder)
+        {
+            if (valueHolder[valueHolder.Length - 1].Equals(default(T)))
+            {
+                return valueHolder;
             }
             else
             {
-                this.listOfElements.Add(element);
-                this.ElementContainer[count] = element;
-                count++;
+                T[] temp = new T[initialValue * 2];
+                for (int i = 0; i < valueHolder.Length; i++)
+                {
+                    temp[i] = valueHolder[i];
+                }
+                initialValue = initialValue * 2;
+                return valueHolder = temp;
             }
+
         }
-        public T GetElementByIndex(int index)
+
+        public static T Min<T>(GenericList<T>array)
         {
-            if (index>ElementContainer.Length)
+            var minValue = default(T);
+            for (var i=0;i<array.Count();i++)
             {
-                throw new IndexOutOfRangeException(String.Format("Maximum count of ements in the collection is {0} !!!",currentCapacity));
+                if (array[i].CompareTo(minValue)>0)
+                {
+                    minValue = item;
+                }
             }
-            return ElementContainer[index];
+            return minValue;
         }
-        public void RemoveElement(int index)
-        {
-            listOfElements.RemoveAt(index);
-            for (int i = index; i < currentCapacity; i++)
-            {
-                ElementContainer[i] = listOfElements[i];
-            }
-        }
+
     }
 }
--- Task2_DefiningClasses/DefiningClasses2/DefiningClasses2/Start.cs
+++ Task2_DefiningClasses/DefiningClasses2/DefiningClasses2/Start.cs
@@ -9,36 +9,44 @@
         {
             Point3D myPoint = new Point3D(35, 14, 3);
             Point3D secondPoint = new Point3D(17, 6, 2);
-            var x = DistanceBetweenPoints.CalculateDistance(myPoint, secondPoint);
-            Path c = new Path();
-            c.AddToPath(secondPoint);
-            c.AddToPath(myPoint);
-            PathStorage.SaveToFile(c);
-            PathStorage.LoadFromFile(c);
+           // var x = DistanceBetweenPoints.CalculateDistance(myPoint, secondPoint);
+           // Path c = new Path();
+           // c.AddToPath(secondPoint);
+           // c.AddToPath(myPoint);
+           // PathStorage.SaveToFile(c);
+           // PathStorage.LoadFromFile(c);
 
-            GenericList<string> genList = new GenericList<string>(4);
+            GenericList<Point3D> genList = new GenericList<Point3D>(3);
 
-            genList.AddElement("Jhon");
-            genList.AddElement("Jhon2");
-            genList.AddElement("Jhon3");
-            genList.AddElement("Jhon4");
-            foreach (var item in genList.listOfElements)
-            {
-                Console.WriteLine(item);
-            }
-            var items = new List<string>();
-            items.Add("Nasko");
-            items.Add("Pesho");
-            items.Add("Rally");
-            items.Add("Minka");
-            foreach (var item in items)
-            {
-                Console.WriteLine(item);
-            }
-            Console.WriteLine(items.Count);
-            items.Remove("Pesho");
-            Console.WriteLine(items.Count);
-            foreach (var item in items)
+
+            genList.AddElement(myPoint);
+            genList.AddElement(secondPoint);
+
+            
+           var nas = genList.Min<Point3D>(myPoint, secondPoint);
+
+           // var x2 = genList.FindElement(new Point3D {X=32,Y=144,Z=92 });
+            Console.WriteLine("++++++++++++++++");
+            //Console.WriteLine(x2);
+            Console.WriteLine("++++++++++++++++");
+            //genList.RemovingElement(0);
+            //genList.InsertElement(1, "naskooo");
+
+
+
+            //genList.RemoveElement(0);
+            // genList.InsertToList(2, "Monster");
+            //
+            // genList.AddElement("ddd");
+            // genList.RemoveElement(1);
+            // genList.RemoveElement(3);
+            // genList.RemoveElement(2);
+            // genList.InsertToList(3, "doncho");
+            // genList.AddElement("nasssssss");
+
+            Console.WriteLine("=====================");
+
+            foreach (var item in genList.elementContainer)
             {
                 Console.WriteLine(item);
             }
